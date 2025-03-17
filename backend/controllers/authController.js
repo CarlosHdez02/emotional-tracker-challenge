da2f821c33630ab870b633d1a0927b93e918a72e
Carlos Hernandez <carloshinzunza2@gmail.com>
2025-03-17 11:13:19 -0600

    removed non using method

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -20,49 +20,6 @@ export default class AuthController {
     }
   }
 
-  async requestPasswordReset(req, res, next) {
-    try {
-      const { email } = req.body;
-      
-      if (!email) {
-        throw new ValidationError('Email is required');
-      }
-
-      // Find user by email
-      const user = await this.userService.findByUserEmail(email)
-      
-      if (!user) {
-        return res.status(200).json({
-          success: true,
-          message: 'If your email exists in our system, you will receive a password reset link'
-        });
-      }
-
-      const resetToken = await this.authService.generateVerificationToken();
-      
-      // Hash token and save to user
-      const hashedToken = crypto
-        .createHash("sha256")
-        .update(resetToken)
-        .digest("hex");
-      
-      user.resetPasswordToken = hashedToken;
-      user.resetPasswordExpire = Date.now() + 15 * 60 * 1000; // 15 minutes
-      await user.save();
-
-      // In a real app, you would send an email with the reset token
-      // For this example, we'll just return the token in the response
-      // In production, you would NOT return the token directly like this
-      
-      res.status(200).json({
-        success: true,
-        message: 'If your email exists in our system, you will receive a password reset link',
-        resetUrl: `${req.protocol}://${req.get('host')}/auth/reset-password/${resetToken}`
-      });
-    } catch (err) {
-      next(err);
-    }
-  }
 
   async resetPassword(req, res, next) {
     try {
